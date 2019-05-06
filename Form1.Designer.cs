7b5950ae66851cd4ae9576e578a73529e831ddbb
Ege Bilecen <egebilecen@icloud.com>
2019-05-06 15:46:39 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace LastEpochSaveProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_export = new System.Windows.Forms.Button();
+            this.btn_import = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::LastEpochSaveProgram.Properties.Resources.logo;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(429, 139);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_export
+            // 
+            this.btn_export.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_export.Location = new System.Drawing.Point(343, 157);
+            this.btn_export.Name = "btn_export";
+            this.btn_export.Size = new System.Drawing.Size(98, 68);
+            this.btn_export.TabIndex = 2;
+            this.btn_export.Text = "Export";
+            this.btn_export.UseVisualStyleBackColor = true;
+            this.btn_export.Click += new System.EventHandler(this.btn_export_Click);
+            // 
+            // btn_import
+            // 
+            this.btn_import.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_import.Location = new System.Drawing.Point(12, 157);
+            this.btn_import.Name = "btn_import";
+            this.btn_import.Size = new System.Drawing.Size(98, 68);
+            this.btn_import.TabIndex = 3;
+            this.btn_import.Text = "Import";
+            this.btn_import.UseVisualStyleBackColor = true;
+            this.btn_import.Click += new System.EventHandler(this.btn_import_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_delete.Location = new System.Drawing.Point(180, 157);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(98, 68);
+            this.btn_delete.TabIndex = 4;
+            this.btn_delete.Text = "Delete Save";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Visible = false;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(366, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "By Zaphiel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 256);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_import);
+            this.Controls.Add(this.btn_export);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Last Epoch Save Importer / Exporter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_export;
+        private System.Windows.Forms.Button btn_import;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace LastEpochSaveProgram
+{
+    public partial class Form1 : Form
+    {
+        readonly string key = @"Software\Eleventh Hour Games\Last Epoch";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DeleteCurrentSave()
+        {
+            RegistryKey regKey = Registry.CurrentUser;
+            regKey.DeleteSubKeyTree(key);
+            regKey.Close();
+        }
+
+        private void btn_import_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Registration file|*.reg";
+            fileDialog.Title  = "Import Save Data";
+
+            if(fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                DeleteCurrentSave();
+
+                using (Process proc = new Process())
+                {
+                    proc.StartInfo.FileName = "regedit.exe";
+                    //proc.StartInfo.UseShellExecute = false;
+                    proc.StartInfo.CreateNoWindow = true;
+                    proc.StartInfo.Arguments = "/s \""+fileDialog.FileName+"\"";
+                    proc.Start();
+                    proc.WaitForExit();
+
+                    MessageBox.Show("Your save is sucessfully imported.", "Info", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        private void btn_export_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog fileDialog = new SaveFileDialog();
+            fileDialog.Filter = "Registration file|*.reg";
+            fileDialog.Title  = "Export Save Data";
+
+            if(fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (Process proc = new Process())
+                    {
+                        proc.StartInfo.FileName = "reg.exe";
+                        proc.StartInfo.UseShellExecute = false;
+                        proc.StartInfo.CreateNoWindow = true;
+                        proc.StartInfo.Arguments = "export \"HKEY_CURRENT_USER\\" + key + "\" \"" + fileDialog.FileName + "\" /y";
+                        proc.Start();
+                        proc.WaitForExit();
+
+                        MessageBox.Show("Your save is sucessfully exported.", "Info", MessageBoxButtons.OK);
+                    }
+                }
+                catch(Exception)
+                {
+                    MessageBox.Show("Unknown error occured.", "Error!", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            DeleteCurrentSave();
+        }
+    }
+}
